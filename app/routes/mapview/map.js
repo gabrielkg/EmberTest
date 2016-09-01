51de7d29e38e9351053e275fde2f044f42bcdf9d
Gabriel Keeble-Gagnere <gabriel.keeble-gagnere@ecodev.vic.gov.au>
2016-09-01 16:13:36 +1000

    Glob the map route to allow multiple maps.

--- app/routes/mapview/map.js
+++ app/routes/mapview/map.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
+    console.log("params to mapview/map route:");
+    console.log(params);
     return this.get('store').findRecord('map', params.map_id);
   }
 });
