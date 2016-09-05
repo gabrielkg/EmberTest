d8c7c498fba7e6b1f26a3b00ffc6c78b49d83f1e
Gabriel Keeble-Gagnere <gabriel.keeble-gagnere@ecodev.vic.gov.au>
2016-09-05 16:26:23 +1000

    Add comment.

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('about');
   this.route('mapview', function() {
     this.route('map', { path: "map/:map_id" });
+    // Wildcard route
     this.route('maps', { path: "maps/:map_id/*maps" });
   });
   this.route('markers');
