0f168c08725ef7f780bfa4aa59541d8312d3c390
Gabriel Keeble-Gagnere <gabriel.keeble-gagnere@ecodev.vic.gov.au>
2016-09-06 11:22:12 +1000

    Add transform to deserialize marker from JSON returned for map.

--- mirage/fixtures/maps.js
+++ mirage/fixtures/maps.js
@@ -1,16 +1,26 @@
-export default [{
-  type: 'maps',
-  id: 1,
-  name: 'Chinese Spring x Renan',
-  size: 10
-}, {
-  type: 'maps',
-  id: 2,
-  name: 'MAGIC CSIRO 8-way',
-  size: 41
-}, {
-  type: 'maps',
-  id: 3,
-  name: 'MAGIC NIAB 8-way',
-  size: 56
-}];
+export default [
+  {
+    id: 1,
+    type: 'maps',
+    name: 'Chinese Spring x Renan',
+    markers: [ ]
+  },
+  {
+    id: 2,
+    type: 'maps',
+    name: 'MAGIC CSIRO 8-way',
+    markers:
+      [
+        { name: 'wsnp_Ku_c8358_14210991', cM: 0 },
+        { name: 'Excalibur_c20311_240', cM: 0.111003484327098 },
+        { name: '7AS_4195134_2629.1', cM: 0.111003484327098 },
+        { name: 'Ra_c2171_1347', cM: 0.829525628357239 }
+      ]
+  },
+  {
+    id: 3,
+    type: 'maps',
+    name: 'MAGIC NIAB 8-way',
+    markers: [ ]
+  }
+];
